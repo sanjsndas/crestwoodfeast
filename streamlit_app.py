fd944c5cec1a8bb1b8321a2e27213b27fae847d5
sanjsndas <jaintipu65658@gmail.com>
2026-06-29 23:31:51 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="My Website",
+    layout="wide"
+)
+
+st.title("Website Hosted on Streamlit")
+
+st.write("Your Streamlit application is running successfully.")
